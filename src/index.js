f158ab9f1eed70753d79343ec2c991b2eabe2392
Zhongming <atenliao@gmail.com>
2023-05-08 21:27:57 -0700

    optimize index.js code

--- src/index.js
+++ src/index.js
@@ -1,56 +1,56 @@
 let geo =[]
-let CityObj={
-    city:'',
-    state:''
-}
+// let WeatherObj
+
 document.addEventListener('DOMContentLoaded',()=>{
-    let state = 'CA'
-    let city = 'San Francisco'
- 
-    
-    // geo.push(getCityLocation(city,state)
-    // if(geo[0] != undefined && geo[1] != undefined){
-    //     console.log('geometry:'+geo[0])
-    // }
-   
-    // 
-   ListStates()
-//    let getstr =
-    getState()
+
+   ListCitys()
+   getCity()
+    // renderWeather(WeatherObj)
 //    console.log(getstr)
 //    document.querySelector('#Display_state').innerText= `we get state ${getstr}`
 })
 
-let gridX=11, gridY=12,startTime,endTime
+// let gridX=11, gridY=12,startTime,endTime
 
-function getWeather(lat, lng){
-    console.log(lat, lng)
-    fetch(`https://api.weather.gov/points/${lat},${lng}`)
-    .then(res=>res.json())
-    .then(e=>{
-        console.log(e.properties)
-      let gridID = e.properties.gridId
-      let gridX = e.properties.gridX
-      let gridY = e.properties.gridY
-      let weatherUrl = `https://api.weather.gov/gridpoints/${gridID}/${gridX},${gridY}/forecast/hourly`
-    //   console.log(weatherUrl)
-    //   let curTime = new Date()
-      fetch(weatherUrl)
-        .then(res=>res.json())
-        .then(e=>{
-            // let len = e.properties.periods.length
-            // console.log(e.properties.periods[len-1])
-            // startTime = e.properties.periods[len-1].startTime;
-            // endTime = e.properties.periods[len-1].endTime;
-            // showMyLocaltime()
-            console.log(e.properties.periods)
-            let index = e.properties.periods.findIndex(findIndex)
-            console.log(index)
-            let WeatherObj = e.properties.periods[index]
-            console.log(WeatherObj.shortForecast)
-        })
+
+function getCity(){
+    const selectCity = document.querySelector('#citys_name')
+    let city
+    const state= 'CA'
+    selectCity.addEventListener('change',(event)=>{
+        event.preventDefault()
+        city = event.target.value
+        // return value
+    document.querySelector('#Display_city').innerText= `the city is ${event.target.value}  `  
+    // document.querySelector('#city_collection').appendChild(city)
+    getCityLocation(city,state)
     })
+    console.log(city)
+    
+}
+async function getCityLocation(city,state){
+    const API_key = ''
+    try{
+        const getres = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city},+${state}&key=${API_key}`)
+        const geoData = await getres.json()
+        let lat = geoData.results[0].geometry.location.lat;
+        let lng = geoData.results[0].geometry.location.lng
 
+        const res = await fetch(`https://api.weather.gov/points/${lat},${lng}`)
+        let e = await res.json()
+        let weatherUrl = `https://api.weather.gov/gridpoints/${e.properties.gridId}/${e.properties.gridX},${e.properties.gridY}/forecast/hourly`
+        const response = await fetch(weatherUrl)
+        let data = await response.json()
+        console.log(data.properties.periods)
+        let index = data.properties.periods.findIndex(findIndex)
+
+        WeatherObj = data.properties.periods[index]
+        console.log(WeatherObj.shortForecast)
+        renderWeather(WeatherObj)
+    //    getWeather(geo[0],geo[1])
+    }catch(error){
+        console.error('fetch city data error:',error)
+    }
     
 }
 
@@ -68,43 +68,9 @@ function findIndex(element){
     }
 }
 
-function showMyLocaltime(){
-    const localTime = new Date();
-    let StartDate = new Date(startTime);
-    let EndDate = new Date(endTime);
-    let localStartTime = new Date(StartDate.toLocaleString('en-US',{timeZone:'America/Los_Angeles'}))
-    let localEndTime = new Date(EndDate.toLocaleString('en-US',{timeZone:'America/Los_Angeles'}))
-   
-    console.log("local time: " + localTime +"start time:" + localStartTime+'end time'+ localEndTime)
-    if(localTime >= localStartTime && localTime <= localEndTime){
-        console.log('the local time match period')
-    }else{
-        console.log('the local time not match period')
-    }
-}
-
-const getCityLocation = (city,state)=>{
-    // let LAT=0, LONG=0
-    // const url = ;
-    // console.log(url)
-    // let CityLoc = []
-    const API_key = ''
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city},+${state}&key=${API_key}`)
-    .then(res => res.json())
-    .then(geoData=>{
-        // console.log(geoData.results[0].geometry.location)
-         geo[0] = geoData.results[0].geometry.location.lat;
-         geo[1] = geoData.results[0].geometry.location.lng
-        //  console.log([LAT,LONG])
-        // console.log(geo)
-        //  return [LAT, LONG]
-        getWeather(geo[0],geo[1])
-    })
-    
-}
 
 
-function ListStates(){
+function ListCitys(){
 
     // StatesArray = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
     cities = [
@@ -140,31 +106,51 @@ function ListStates(){
         "Elk Grove"
     ]
     
-    let StateOption =[] 
+    let CitysOption =[] 
     let i=0
-    const stateName = document.querySelector('#state_name')
-    cities.forEach(state=>{
-        StateOption[i]=document.createElement('option')
-        StateOption[i].value = state
-        StateOption[i].innerText= state
-        stateName.appendChild(StateOption[i])
+    const cityName = document.querySelector('#citys_name')
+    cities.forEach(city=>{
+        CitysOption[i]=document.createElement('option')
+        CitysOption[i].value = city
+        CitysOption[i].innerText= city
+        cityName.appendChild(CitysOption[i])
         i++
     })
     
 
 }
 
-function getState(){
-    const selectCity = document.querySelector('#new_state')
-    let city
-    const state= 'CA'
-    selectCity.addEventListener('change',(event)=>{
-        event.preventDefault()
-        city = event.target.value
-        // return value
-    document.querySelector('#Display_state').innerText= `the state is ${event.target.value}  `  
-    getCityLocation(city,state)
-    })
-    console.log(city)
-    
+let forecastPhoto
+
+function renderWeather(cityWeatherObj){
+    const cityCard = document.createElement('div');
+    switch(cityWeatherObj.shortForecast){
+        case 'Sunny':
+            forecastPhoto = './assets/sun.png'
+            break;
+        case 'Mostly Sunny':
+            forecastPhoto = './assets/suncloud.png'
+        case 'Partly Cloudy':
+            forecastPhoto = './assets/suncloud.png'
+            break;
+        case 'Mostly Cloudy':
+            forecastPhoto = './assets/Moonloud.png'
+            break;
+        case 'cloudy':
+            forecastPhoto = './assets/suncloud.png'
+            break;
+        case 'Mostly Clear':
+            forecastPhoto = './assets/Mooncloud.png'
+            break
+        case 'Clear':
+            forecastPhoto = './assets/Moon.png'
+    }
+    cityCard.className = 'citycard';
+    cityCard.innerHTML=`
+        <img src = ${forecastPhoto} class = 'forecastphoto' style="margin:0 0 0 0" width='300' height='200'>
+        <p>${cityWeatherObj.temperature} &deg${cityWeatherObj.temperatureUnit}</p>
+        <p>${cityWeatherObj.shortForecast}</p>
+        <p>${cityWeatherObj.windSpeed} ${cityWeatherObj.windDirection}<p>
+    `
+    document.querySelector("#city_collection").appendChild(cityCard)
 }
