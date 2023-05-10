cf5d8a57de7c580d6d7c338fda43225d3f435cff
Zhongming <atenliao@gmail.com>
2023-05-10 16:53:44 -0700

    add delete button

--- src/index.js
+++ src/index.js
@@ -1,53 +1,48 @@
-let geo =[]
+let geo = []
 // let WeatherObj
-let weatherObj={
-    city:"",
-    image:"",
+let weatherObj = {
+    city: "",
+    image: "",
     shortForecast: "",
-    windSpeed:"",
-    windDirection:"",
-    temperature:"",
-    humidity:"",
-    gridId:"",
+    windSpeed: "",
+    windDirection: "",
+    temperature: "",
+    humidity: "",
+    gridId: "",
     gridX: 0,
     gridY: 0,
     idName: "",
     count: 0
 }
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
     showCards()
-   ListCitys()
-getCity()
-setInterval(callFunc,300000)
-// setInterval(showCards,10000)
-//    setInterval(updateCard,10000)
-    // CreaterenderWeather(WeatherObj)
-//    console.log(getstr)
-//    document.querySelector('#Display_state').innerText= `we get state ${getstr}`
+    ListCitys()
+    getCity()
+    setInterval(callFunc,300000)
+   
 })
 
-// let gridX=11, gridY=12,startTime,endTime
 
 
-function getCity(){
+
+function getCity() {
     const selectCity = document.querySelector('#citys_name')
     let city
-    
-    selectCity.addEventListener('change',(event)=>{
+
+    selectCity.addEventListener('change', (event) => {
         event.preventDefault()
         city = event.target.value
         // return value
-    document.querySelector('#Display_city').innerText= `the city is ${event.target.value}  `  
-    // document.querySelector('#city_collection').appendChild(city)
-    getCityLocation(city)
+        document.querySelector('#Display_city').innerText = `the city is ${event.target.value}  `
+        // document.querySelector('#city_collection').appendChild(city)
+        getCityLocation(city)
     })
-    // console.log(city)
-    
+
 }
-async function getCityLocation(city){
-    const state= 'CA'
+async function getCityLocation(city) {
+    const state = 'CA'
     const API_key = ''
-    try{
+    try {
         const getres = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city},+${state}&key=${API_key}`)
         const geoData = await getres.json()
         let lat = geoData.results[0].geometry.location.lat;
@@ -59,36 +54,35 @@ async function getCityLocation(city){
         let weatherUrl = `https://api.weather.gov/gridpoints/${e.properties.gridId}/${e.properties.gridX},${e.properties.gridY}/forecast/hourly`
         const response = await fetch(weatherUrl)
         let data = await response.json()
-        // console.log(data.properties.periods)
         let index = data.properties.periods.findIndex(findIndex)
 
         let WeatherObj = data.properties.periods[index]
         console.log(WeatherObj.shortForecast)
-        CreaterenderWeather(WeatherObj,city,e.properties.gridId,e.properties.gridX,e.properties.gridY)
-    
-    }catch(error){
-        console.error('fetch city data error:',error)
+        CreaterenderWeather(WeatherObj, city, e.properties.gridId, e.properties.gridX, e.properties.gridY)
+
+    } catch (error) {
+        console.error('fetch city data error:', error)
     }
-    
+
 }
 
-function findIndex(element){
+function findIndex(element) {
     let curtime = new Date()
     let StartDate = new Date(element.startTime);
     let EndDate = new Date(element.endTime);
-    let localStartTime = new Date(StartDate.toLocaleString('en-US',{timeZone:'America/Los_Angeles'}))
-    let localEndTime = new Date(EndDate.toLocaleString('en-US',{timeZone:'America/Los_Angeles'}))
+    let localStartTime = new Date(StartDate.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }))
+    let localEndTime = new Date(EndDate.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }))
     // console.log("local time: " + curtime +"start time:" + localStartTime+'end time'+ localEndTime)
-    if(curtime >= localStartTime && curtime <= localEndTime){
+    if (curtime >= localStartTime && curtime <= localEndTime) {
         return true
-    }else{
+    } else {
         return false
     }
 }
 
 
 
-function ListCitys(){
+function ListCitys() {
 
     // StatesArray = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
     cities = [
@@ -132,25 +126,25 @@ function ListCitys(){
         "Ontario",
         "Elk Grove"
     ]
-    
-    let CitysOption =[] 
-    let i=0
+
+    let CitysOption = []
+    let i = 0
     const cityName = document.querySelector('#citys_name')
-    cities.forEach(city=>{
-        CitysOption[i]=document.createElement('option')
+    cities.forEach(city => {
+        CitysOption[i] = document.createElement('option')
         CitysOption[i].value = city
-        CitysOption[i].innerText= city
+        CitysOption[i].innerText = city
         cityName.appendChild(CitysOption[i])
         i++
     })
-    
+
 
 }
 
 
-function getphoto(shortForecast,isDaytime){
+function getphoto(shortForecast, isDaytime) {
     let forecastPhoto
-    switch(shortForecast){
+    switch (shortForecast) {
         case 'Sunny':
             forecastPhoto = './assets/day.svg'
             break;
@@ -161,24 +155,23 @@ function getphoto(shortForecast,isDaytime){
             forecastPhoto = './assets/cloudy-day-2.svg'
             break;
         case 'Partly Cloudy':
-            if (isDaytime){
+            if (isDaytime) {
                 forecastPhoto = './assets/cloudy-day-2.svg'
-            }else{
+            } else {
                 forecastPhoto = './assets/cloudy-night-2.svg'
             }
-            
             break;
         case 'Mostly Cloudy':
-            if (isDaytime){
+            if (isDaytime) {
                 forecastPhoto = './assets/cloudy-day-3.svg'
-            }else{
+            } else {
                 forecastPhoto = './assets/cloudy-night-3.svg'
             }
             break;
         case 'Cloudy':
-            if (cityWeatherObj.isDaytime){
+            if (cityWeatherObj.isDaytime) {
                 forecastPhoto = './assets/cloudy.svg'
-            }else{
+            } else {
                 forecastPhoto = './assets/cloudy.svg'
             }
             break;
@@ -189,16 +182,16 @@ function getphoto(shortForecast,isDaytime){
             forecastPhoto = './assets/night.svg'
             break;
         case 'Patchy Drizzle':
-            if (isDaytime){
+            if (isDaytime) {
                 forecastPhoto = './assets/cloud-day-3.svg'
-            }else{
+            } else {
                 forecastPhoto = './assets/cloud-night-3.svg'
             }
             break;
         case 'Slight Chance Light Rain':
-            if (isDaytime){
+            if (isDaytime) {
                 forecastPhoto = './assets/rainy-2.svg'
-            }else{
+            } else {
                 forecastPhoto = './assets/rainy-4.svg'
             }
             break;
@@ -206,78 +199,68 @@ function getphoto(shortForecast,isDaytime){
             forecastPhoto = './assets/rainy-7.svg'
             break;
         case 'Thunderstorms':
-            forecastPhoto ='./assets/thunder.svg'
+            forecastPhoto = './assets/thunder.svg'
             break;
     }
     return forecastPhoto
 }
 
-async function CreaterenderWeather(cityWeatherObj, cityName,GridId,GridX,GridY){
-    
-    let forecastPhoto = getphoto(cityWeatherObj.shortForecast,cityWeatherObj.isDaytime)
+async function CreaterenderWeather(cityWeatherObj, cityName, GridId, GridX, GridY) {
+
+    let forecastPhoto = getphoto(cityWeatherObj.shortForecast, cityWeatherObj.isDaytime)
     console.log(forecastPhoto)
     let HaveItem = false
     let getItem = null
-    try{
+    try {
         const res = await fetch("http://localhost:3000/weatherCards")
         let data = await res.json()
         getItem = data.find(function (item) {
             return item.city === cityName
-          })
-        if(getItem){
+        })
+        if (getItem) {
             HaveItem = true
         }
-        
+
         // console.log(HaveItem)
         // console.log(getItem)
-    }catch (error){
+    } catch (error) {
         console.log(error.message)
     }
-    weatherObj={
-        city:cityName,
-        image:forecastPhoto,
+    weatherObj = {
+        city: cityName,
+        image: forecastPhoto,
         shortForecast: cityWeatherObj.shortForecast,
-        windSpeed:cityWeatherObj.windSpeed,
-        windDirection:cityWeatherObj.windDirection,
-        temperature:cityWeatherObj.temperature,
-        humidity:cityWeatherObj.relativeHumidity.value,
+        windSpeed: cityWeatherObj.windSpeed,
+        windDirection: cityWeatherObj.windDirection,
+        temperature: cityWeatherObj.temperature,
+        humidity: cityWeatherObj.relativeHumidity.value,
         gridId: GridId,
         gridX: GridX,
         gridY: GridY,
         count: 1,
     }
 
-    if(HaveItem){
-        
-        getItem.image = forecastPhoto
-        getItem.shortForecast = cityWeatherObj.shortForecast
-        getItem.windSpeed = cityWeatherObj.windSpeed
-        getItem.windDirection = cityWeatherObj.windDirection
-        getItem.temperature = cityWeatherObj.temperature
-        getItem.humidity = cityWeatherObj.relativeHumidity.value
-        getItem.count++
-        console.log('the getItem is:'+getItem.count)
+    if (HaveItem) {
         updateWeatherCard(getItem)
-    }else{
+    } else {
         renderWeather(weatherObj)
         addWeatherCard(weatherObj)
-        
+
     }
-    
+
 }
 
 
 
-function renderWeather(weatherObj){
+function renderWeather(weatherObj) {
     const cityCard = document.createElement('div');
     cityCard.className = 'citycard';
     let unit = 'F'
     let idName = weatherObj.city
-    idName = idName.replace(" ",'_')
+    idName = idName.replace(" ", '_')
     weatherObj.idName = idName
     console.log(idName)
-    cityCard.innerHTML=`
-        
+    cityCard.innerHTML = `
         <div id=${idName}>
         <div class="degrees"><img src = ${weatherObj.image} class = 'forecastphoto'  >${weatherObj.temperature}&deg${unit}</div>
         <div class="place">${weatherObj.city}</div>
@@ -286,115 +269,133 @@ function renderWeather(weatherObj){
         <div class="humidity">Humidity: ${weatherObj.humidity}% </div>
         <div class="count">the count is: ${weatherObj.count}</div>
         </div>
+        <button id="clean" class="clean">x</button>
         `
 
     document.querySelector("#city_collection").appendChild(cityCard)
 }
 
 
-function addWeatherCard(WeatherObj){
-    fetch("http://localhost:3000/weatherCards",{
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json",
-            Accept:"application/json"
+function addWeatherCard(WeatherObj) {
+    fetch("http://localhost:3000/weatherCards", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
         },
         body: JSON.stringify(WeatherObj)
-        })
-        .then(res=>res.json)
-        .then(item => console.log(item))
-        .catch(error=>console.log(error.message))
-
-}
-
-function updateWeatherCard(WeatherObj){
-    fetch(`http://localhost:3000/weatherCards/${WeatherObj.id}`,{
-        method:"PATCH",
-        headers:{
-            "Content-Type":"application/json",
-            Accept:"application/json"
-        },
-        body:JSON.stringify(WeatherObj)
     })
-    
+        .then(res => res.json)
+        .then(item => console.log(item))
+        .catch(error => console.log(error.message))
+
 }
 
-async function showCards(){
-    try{
+async function updateWeatherCard(WeatherObj) {
+    fetch(`http://localhost:3000/weatherCards/${WeatherObj.id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(WeatherObj)
+    })
+
+}
+
+async function showCards() {
+    try {
         const res = await fetch("http://localhost:3000/weatherCards")
         let weatherCards = await res.json()
         // console.log(weatherCards)
-        weatherCards.forEach(card=> renderWeather(card))
-    }catch(error){
+        weatherCards.forEach(card => renderWeather(card))
+    } catch (error) {
         console.log(error.message)
     }
-    
+
 }
 
-function UpdateddataBase(TheweatherObj){
-        let unit = 'F'
-        let element = document.querySelector(`#${TheweatherObj.idName}`)
-        console.log('element id: '+element.id)
-        // const cityId = document.querySelector(element.id)
-        // console.log("cityId:",cityId)
-        const degree = element.querySelector(".degrees")
-        const place = element.querySelector(".place")
-        const weather = element.querySelector(".weather")
-        const wind = element.querySelector(".wind")
-        const humidity = element.querySelector(".humidity")
-        const count = element.querySelector(".count")
-        
-        // const palce = city_collection.getElementsByClassName("place")
-        degree.innerHTML=`<img src = ${TheweatherObj.image} class = 'forecastphoto'  >${TheweatherObj.temperature}&deg${unit}`
-        
-        weather.textContent=`${TheweatherObj.shortForecast}`
-        wind.textContent = `Wind: ${TheweatherObj.windSpeed} | Direction: ${TheweatherObj.windDirection}`
-        humidity.textContent=`Humidity: ${TheweatherObj.humidity}% `
+async function UpdateddataBase(weatherObj) {
+    let weatherUrl = `https://api.weather.gov/gridpoints/${weatherObj.gridId}/${weatherObj.gridX},${weatherObj.gridY}/forecast/hourly`
+    const response = await fetch(weatherUrl)
+    let data = await response.json()
+    console.log(weatherObj.city)
+    // console.log(data.properties.periods)
+    let index = data.properties.periods.findIndex(findIndex)
 
-        count.textContent=`the count is ${TheweatherObj.count}`
+    let WeatherElement = data.properties.periods[index]
+    console.log(WeatherElement)
+    console.log('ShortForecast',WeatherElement.shortForecast)
+    let forecastPhoto = getphoto(WeatherElement.shortForecast, WeatherElement.isDaytime)
+    weatherObj.image = forecastPhoto
+    weatherObj.shortForecast = WeatherElement.shortForecast
+    weatherObj.windSpeed = WeatherElement.windSpeed
+    weatherObj.windDirection = WeatherElement.windDirection
+    weatherObj.temperature = WeatherElement.temperature
+    weatherObj.humidity = WeatherElement.relativeHumidity.value
+    weatherObj.count++
+    console.log(WeatherElement.shortForecast)
+    let unit = 'F'
+    let element = document.querySelector(`#${weatherObj.idName}`)
+    console.log('element id: ' + element.id)
 
-        console.log('count:',count.textContent)
-        // console.log('textContent:'+ count.textContent)
-        console.log('The weatherObj count: '+ TheweatherObj.count)
-        
-   
+    const degree = element.querySelector(".degrees")
+    // const place = element.querySelector(".place")
+    const weather = element.querySelector(".weather")
+    const wind = element.querySelector(".wind")
+    const humidity = element.querySelector(".humidity")
+    const count = element.querySelector(".count")
+
+    degree.innerHTML = `<img src = ${forecastPhoto} class = 'forecastphoto'  >${WeatherElement.temperature}&deg${unit}`
+    weather.textContent = `${WeatherElement.shortForecast}`
+    wind.textContent = `Wind: ${WeatherElement.windSpeed} | Direction: ${WeatherElement.windDirection}`
+    humidity.textContent = `Humidity: ${WeatherElement.relativeHumidity.value}% `
+    count.innerHTML = `the count is ${weatherObj.count}`
+    // console.log('count:', count.textContent)
+    // console.log('The weatherObj count: ' + WeatherElement.count)
+
+
+    updateWeatherCard(weatherObj)
+
 }
 
-async function updateCard(){
-    try{
+async function updateCard() {
+    try {
         const res = await fetch("http://localhost:3000/weatherCards")
         let weatherCards = await res.json()
         console.log(weatherCards)
-        weatherCards.forEach(card=> UpdateBase(card))
-    }catch(error){
+        weatherCards.forEach(card => UpdateBase(card))
+    } catch (error) {
         console.log(error.message)
     }
 }
 
 
-async function updateCityWeatherObj(){
+async function updateCityWeatherObj() {
 
-    try{
+    try {
         const res = await fetch("http://localhost:3000/weatherCards")
         let weatherCards = await res.json()
         console.log(weatherCards)
-        weatherCards.forEach(card=> getCityLocation(card.city))
-    }catch(error){
+        weatherCards.forEach(weatherObj => updateCityWeather(weatherObj))
+    } catch (error) {
         console.log(error.message)
     }
-    
+
 }
 
 
-async function callFunc(){
+
+
+async function callFunc() {
     updateCityWeatherObj()
-    try{
+    try {
         const res = await fetch("http://localhost:3000/weatherCards")
         let weatherCards = await res.json()
-        console.log('before updated database: ',weatherCards)
+        console.log('before updated database: ', weatherCards)
         weatherCards.forEach(card => UpdateddataBase(card))
-    }catch(error){
+    } catch (error) {
         console.log(error.message)
-        
+
     }
 }
